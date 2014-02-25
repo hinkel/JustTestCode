7bad300ec218d19d3a6524b145992273b53da7f9
hinkel <hinkel@users.noreply.github.com>
2014-02-25 19:06:05 +0100

    Update drv_i2c.c
    I2C_ClockSpeed = 100000 for MB1242

--- src/drv_i2c.c
+++ src/drv_i2c.c
@@ -306,7 +306,7 @@ void i2cInit(I2C_TypeDef *I2C)
     I2C_InitStructure.I2C_Mode = I2C_Mode_I2C;
     I2C_InitStructure.I2C_DutyCycle = I2C_DutyCycle_2;
     I2C_InitStructure.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
-    I2C_InitStructure.I2C_ClockSpeed = 400000;
+    I2C_InitStructure.I2C_ClockSpeed = 400000;  // = 100000; for MB1242 
     I2C_Cmd(I2Cx, ENABLE);
     I2C_Init(I2Cx, &I2C_InitStructure);
 
